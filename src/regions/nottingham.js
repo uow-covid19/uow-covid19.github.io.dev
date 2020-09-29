df4fad89e84d78c5d779a5a8f1b4d20df84d5e2f
uow-covid-tracker <71976713+uow-covid-tracker@users.noreply.github.com>
2020-09-29 17:03:24 +0100

    Add Nottingham (#3)

---
+++ src/regions/nottingham.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+// Used as the website title and header.
+export const LONG_NAME = "University of Nottingham"
+export const SHORT_NAME = "Notts"
+
+// A list of the msoa11_cd values for regions in the dataset
+// (e.g. E02000001 for City of London)
+export const REGION_CODES = [
+  'E02002899', // University Park, Lenton Abbey & Jubilee Campus
+  'E02002894', // Wollaton Park
+  'E02002898', // Lenton & Dunkirk
+  'E02002893', // Radford
+  'E02002887', // Beechdale
+  'E02005860', // Beeston Town
+  'E02005398' // Kegworth & Belton
+]
+
+export const EXTRA_LINKS = [
+  <li><a href='https://forms.office.com/Pages/ResponsePage.aspx?id=7qe9Z4D970GskTWEGCkKHh6YksPWJp5EnnJQcOvHFxxUQ1ZPVVJJU0RJTDJITFYxVzVSQ1dUT0ZDTyQlQCN0PWcu'>Report Symptoms</a></li>,
+  <li><a href='https://forms.office.com/Pages/ResponsePage.aspx?id=7qe9Z4D970GskTWEGCkKHkrlWsFnDzFNtR-2zavZvFVUNzQ4N0NVSktQNUI2U0pXWUxORUg0TEgwVCQlQCN0PWcu'>Report an incident</a></li>,
+  <li><a href='mailto:ss-student-selfisol@nottingham.ac.uk'>Email the Helpline</a>,
+]
