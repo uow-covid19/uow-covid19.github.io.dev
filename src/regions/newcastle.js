8bb02dbedca0a45d5665f92d7dcc9669462aaab0
uow-covid-tracker <71976713+uow-covid-tracker@users.noreply.github.com>
2020-09-29 16:07:25 +0100

    Add Newcastle (#1)

---
+++ src/regions/newcastle.js
@@ -0,0 +1,22 @@
+// import React from 'react'
+
+// Used as the website title and header.
+export const LONG_NAME = "University of Newcastle"
+export const SHORT_NAME = "UoN"
+
+// A list of the msoa11_cd values for regions in the dataset
+// (e.g. E02000001 for City of London)
+export const REGION_CODES = [
+  'E02001730', // Shieldfield & Heaton Park
+  'E02001731', // City Centre & Arthur's Hill
+  'E02001736', // Elswick,
+  'E02001724', // South Jesmond & Sandyford
+  'E02001720', // North Jesmond
+  'E02001719', // North Heaton
+  'E02001725', // Heaton South
+  'E02001733', // Byker
+  'E02001714', // High Heaton
+]
+
+export const EXTRA_LINKS = [
+]
