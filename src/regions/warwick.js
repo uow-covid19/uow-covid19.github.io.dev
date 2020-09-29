72749ad85d58861a7edf1f85abce72a315d8f0b2
uow-covid19 <person@example.com>
2020-09-29 12:23:30 +0100

    CD changes

---
+++ src/regions/warwick.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+// Used as the website title and header.
+export const LONG_NAME = "University of Warwick"
+export const SHORT_NAME = "UoW"
+
+// A list of the msoa11_cd values for regions in the dataset
+// (e.g. E02000001 for City of London)
+export const REGION_CODES = [
+  'E02001999', // Campus
+  'E02006523', // Cubbington, Stoneleigh & Radford Semele
+  'E02001993', // Canley and Westwood Heath
+  'E02001986', // Tile Hill
+  'E02006525', // Leamington Central & North
+  'E02006528', // Leamington East
+  'E02006531', // Leamington Brunswick
+  'E02006527', // Leamington West
+  'E02006521', // Kenilworth South
+  'E02001991'  // Earlsdon & Canley Gardens
+]
+
+export const EXTRA_LINKS = [
+  <li><a href="https://testandtrace.warwick.ac.uk/intro/">Warwick Test and Trace</a></li>,
+  <li>Warwick 24-Hour Helpline: +44 24 7652 3111</li>
+]
