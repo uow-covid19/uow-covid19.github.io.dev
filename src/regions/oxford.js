3f4c3c050674405e15988a39d0543c6212df49ba
uow-covid-tracker <71976713+uow-covid-tracker@users.noreply.github.com>
2020-09-29 16:28:58 +0100

    Add Oxford (#2)

---
+++ src/regions/oxford.js
@@ -0,0 +1,24 @@
+// import React from 'react'
+
+// Used as the website title and header.
+export const LONG_NAME = "University of Oxford"
+export const SHORT_NAME = "Ox"
+
+// A list of the msoa11_cd values for regions in the dataset
+// (e.g. E02000001 for City of London)
+export const REGION_CODES = [
+  'E02005950', // East Central Oxford
+  'E02005947', // Oxford Central
+  'E02005945', // Headington
+  'E02005953', // Iffley Fields
+  'E02005952', // Cowley North
+  'E02005954', // Cowley South & Iffley
+  'E02005943', // Marston
+  'E02005942', // North Central Oxford
+  'E02005941', // Summertown
+  'E02005940', // Wolvercote & Cutteslowe
+]
+
+export const EXTRA_LINKS = [
+  <li><a href='https://earlyalert.medsci.ox.ac.uk/'>Book a Test</a></li>
+]
