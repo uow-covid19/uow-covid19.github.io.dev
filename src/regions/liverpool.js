e2fceec6734ce34680fc18b0bb5ed450d5d3a2a5
uow-covid19 <person@example.com>
2020-09-29 12:40:49 +0100

    more CD changes

---
+++ src/regions/liverpool.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+// Used as the website title and header.
+export const LONG_NAME = "University of Liverpool"
+export const SHORT_NAME = "UoL"
+
+// A list of the msoa11_cd values for regions in the dataset
+// (e.g. E02000001 for City of London)
+export const REGION_CODES = [
+  'E02006932', // Central and Islington
+  'E02001381', // Wavertree West
+  'E02001384', // Wavertree South
+  'E02001388', // Wavertree Penny Lane
+  'E02001377', // Edge Hill
+  'E02001369', // Everton West
+  'E02006934', // Pier Head
+  'E02001383'  // Chinatown, St James & Georgian Quarter
+]
+
+export const EXTRA_LINKS = [
+    <li><a href='https://jbsgeneral.simplybook.pro/v2/'>Book a Test</a></li>,
+]
---
+++ src/regions/manchester.js
@@ -0,0 +1,24 @@
+// import React from 'react'
+
+// Used as the website title and header.
+export const LONG_NAME = "University of Manchester / Manchester Met"
+export const SHORT_NAME = "UoM/MMU"
+
+// A list of the msoa11_cd values for regions in the dataset
+// (e.g. E02000001 for City of London)
+export const REGION_CODES = [
+  'E02006914', // University North & Whitworth Street
+  'E02001063', // Hulme & University
+  'E02001062', // Ardwick
+  'E02001259', // Old Trafford
+  'E02001069', // Rusholme West & Moss Side East
+  'E02001067', // Gorton South
+  'E02001071', // Levenshulme North
+  'E02001075', // Levenshulme Central
+  'E02001078', // Levenshulme South & Burnage North
+  'E02001080', // Ladybarn
+  'E02001067'  // Gorton South
+]
+
+export const EXTRA_LINKS = [
+]
