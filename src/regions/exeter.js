d94a68c389deb17cfd82341e81c9feb14d0be43d
uow-covid19 <person@example.com>
2020-09-30 10:18:24 +0100

    Add Exeter

---
+++ src/regions/exeter.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+// Used as the website title and header.
+export const LONG_NAME = "University of Exeter"
+export const SHORT_NAME = "Exeter"
+
+// A list of the msoa11_cd values for regions in the dataset
+// (e.g. E02000001 for City of London)
+export const REGION_CODES = [
+  'E02004149', // Pennsylvania & University
+  'E02004152', // St James's Park & Hoopern
+  'E02004156', // Central Exeter
+  'E02004153', // Exwick & Foxhayes
+  'E02004151', // Pinhoe & Whipton North
+  'E02004154', // Heavitree West & Polsloe
+  'E02004150', // Mincinglake & Beacon Heath
+  'E02004158', // St Thomas East
+  'E02004160', // St Thomas West
+  'E02004162', // Alphington & Marsh Barton
+  'E02004161'  // Wonford & St Loye's
+]
+
+export const EXTRA_LINKS = [
+    <li><a href='https://jbsgeneral.simplybook.pro/v2/'>Book a Test</a></li>,
+]
